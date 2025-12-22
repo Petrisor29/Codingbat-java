8cc5a42e463d6eac3056026db2dec8f855624155
Petrisor29 <petrisor.gnr@gmail.com>
2025-12-23 00:32:21 +0200

    Add MapAB3 class with mapAB3 method
    Implement mapAB3 method to synchronize values of keys 'a' and 'b' in a map.

---
+++ src/map1/MapAB3.java
@@ -0,0 +1,46 @@
+package map1;
+
+import java.util.Map;
+import java.util.HashMap;
+
+/**
+ * CodingBat - Map-1: mapAB3
+ *
+ * Modify and return the given map as follows: if exactly one of the keys 
+ * "a" or "b" has a value in the map (but not both), set the other 
+ * to have that same value in the map.
+ */
+public class MapAB3 {
+
+    public Map<String, String> mapAB3(Map<String, String> map) {
+        if (map.containsKey("a") && !map.containsKey("b")) {
+            map.put("b", map.get("a"));
+        } else if (!map.containsKey("a") && map.containsKey("b")) {
+            map.put("a", map.get("b"));
+        }
+        return map;
+    }
+
+    public static void main(String[] args) {
+        MapAB3 mAB3 = new MapAB3();
+
+        // Test 1: {"a": "aaa", "c": "cake"}
+        Map<String, String> map1 = new HashMap<>();
+        map1.put("a", "aaa");
+        map1.put("c", "cake");
+        System.out.println(mAB3.mapAB3(map1)); // {a=aaa, b=aaa, c=cake}
+
+        // Test 2: {"b": "bbb", "c": "cake"}
+        Map<String, String> map2 = new HashMap<>();
+        map2.put("b", "bbb");
+        map2.put("c", "cake");
+        System.out.println(mAB3.mapAB3(map2)); // {a=bbb, b=bbb, c=cake}
+
+        // Test 3: {"a": "aaa", "b": "bbb", "c": "cake"}
+        Map<String, String> map3 = new HashMap<>();
+        map3.put("a", "aaa");
+        map3.put("b", "bbb");
+        map3.put("c", "cake");
+        System.out.println(mAB3.mapAB3(map3)); // {a=aaa, b=bbb, c=cake}
+    }
+}
